d4ec0269d80ec78c9238d01790d276ae87ccb11e
Nadine Nam <94868412+dnam912@users.noreply.github.com>
2024-11-01 00:19:02 +0000

    succeed producing items in producer.cpp

--- producer_2.cpp
+++ producer_2.cpp
@@ -1,4 +1,4 @@
-// GLOBAL HEADER FILES
+// GLOBAL INCLUDE FILES
 #include <iostream>
 #include <queue>
 #include <mutex>
@@ -17,20 +17,19 @@ using namespace std;
 // BOUNDED BUFFER
 const char* SHARED_MEM_NAME = "/my_shared_memory";
 const int BUFFER_SIZE = 2; // The table can only hold two items at the same time.
-int index_counter;
 
+// Assign 1 to one of parameters named 'pshared' when initializing semaphores
+int pshared = 1; 
+
+// INITIALIZE MAP FOR A BUFFER IN A SHARED MEMORY
 struct SharedMemory {
     int buffer[BUFFER_SIZE];
     int index_counter;
-    int producer_in = 0;
+    int producer_in;
 
     // INITIALIZE SEMAPHORES
     sem_t mutex, full, empty;
 };
-
-
-// int shm_unlink(const char *name);
-int pshared = 1;
 
 
 void produce() {
@@ -38,63 +37,68 @@ void produce() {
     // int shm_open(const char *name, int oflag, mode_t mode);
     int shm_object = shm_open(SHARED_MEM_NAME, O_CREAT | O_RDWR, 0666);
     
-    // Decide the size of shared mem.
+    // DECIDE THE SIZE OF SHARED MEM
     ftruncate(shm_object, 1024);
 
-    // Map the shared_mem object into the process's address space
-    SharedMemory* SHARED_MEM = (SharedMemory*)mmap(0, sizeof(SharedMemory), PROT_READ | PROT_WRITE, MAP_SHARED, shm_object, 0);
+    // Map the SHARED_MEM into the process's address space
+    SharedMemory* SHARED_MEM = (SharedMemory*)mmap(0, sizeof(SharedMemory),
+                                PROT_READ | PROT_WRITE, MAP_SHARED, shm_object, 0);
+
+    // Assign zero in variables of SHARED MEM
     SHARED_MEM->index_counter = 0;
-    
+    SHARED_MEM->producer_in = 0;
+
     // INITIALIZE SEMAPHORES: sem_init(sem_t *sem, int pshared, unsigned value);
     sem_init(&SHARED_MEM->mutex, pshared, 1);           // sem_mutex initialized to value 1 (binary semaphore)
     sem_init(&SHARED_MEM->full, pshared, 0);            // sem_full initailzied to value 0 (counting semaphore)
     sem_init(&SHARED_MEM->empty, pshared, BUFFER_SIZE); // sem_empty initialized to value n (counting sempaphore)
 
+    for (int i = 0; i < 2; i++) {
+        cout << "DEBUG_1: Assign i into 'buffer[producer_in]" << endl;
+        SHARED_MEM->buffer[SHARED_MEM->producer_in] = i;
 
-    // do {
-        for (int i = 0; i < 2; i++) {
-            cout << "DEBUG_1" << endl;
+        cout << "Produced: " << SHARED_MEM->buffer[SHARED_MEM->producer_in] << endl;
+            
+        SHARED_MEM->index_counter++; // Increment counter of current buffer index
+    }
 
-            SHARED_MEM->buffer[SHARED_MEM->producer_in] = i;
-            cout << "Produced " << SHARED_MEM->buffer[SHARED_MEM->producer_in] << endl;
-            SHARED_MEM->index_counter++; // Increment counter of current buffer index
-        }
-
-        /*
-        // produce an item
-
-        wait(empty); // 
-        wait(mutex); // 
-        
-        // add to buffer
-
-        signal(mutex); // 
-        signal(full); // 
-        */
-
+    // DESTROY ASSIGNED SEMAPHORES
     sem_destroy(&SHARED_MEM->mutex);
     sem_destroy(&SHARED_MEM->full);
     sem_destroy(&SHARED_MEM->empty);
-    munmap(&SHARED_MEM, sizeof(SharedMemory)); // Remove the map containing the address space of process
 
-    //close(shm_object);
+    // REMOVE THE MAP CONTAINING THE ADDRESS SPACE OF PROCESS
+    munmap(&SHARED_MEM, sizeof(SharedMemory)); 
 
+
+    // do {
+        /*
+        // produce an item //
+
+        wait(empty);
+        wait(mutex);
+        
+        // add to buffer //
+
+        signal(mutex);
+        signal(full);
+        */
     // } while (true);
+
 }
 
 int main() {
-    cout << "DEBUG_2: producer()" << endl;
     produce();
-
-    cout << "DEBUG_3 sem_unlink" << endl;
-
+    cout << "DEBUG_2: producer()" << endl;
     
-    
-    sem_unlink(SHARED_MEM_NAME);
+    // UNLINK SHARED MEMORY OBJECT
+    // int shm_unlink(const char *name);
+    shm_unlink(SHARED_MEM_NAME);
+    cout << "DEBUG_3: shm_unlink" << endl;
+
 
     // Fork to create processes (producer & consumer)
     // pid_t pid = fork();
-
     /* PROCESSES
             (pid)
             /   \
